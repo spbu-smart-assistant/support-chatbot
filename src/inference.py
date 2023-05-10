6776055ef08b569724a700ccd37dec17643bd416
Lokis Aleksandr <109301202+Abletobetable@users.noreply.github.com>
2023-05-10 20:22:56 +0300

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,8 +1,42 @@
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 from librosa import load, resample
 import soundfile as sf
 import nemo.collections.asr as nemo_asr
 
-# init model
+# init summa model an tokenizer
+MODEL_NAME = 'cointegrated/rut5-base-absum'
+summa_model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+summa_model.cuda() if torch.cuda.is_available() else None
+summa_model.eval();
+
+def summarize(
+    text, n_words=None, compression=None,
+    max_length=1500, num_beams=3, do_sample=False, repetition_penalty=10.0, 
+    **kwargs
+):
+    """
+    Summarize the text
+    The following parameters are mutually exclusive:
+    - n_words (int) is an approximate number of words to generate.
+    - compression (float) is an approximate length ratio of summary and original text.
+    """
+    if n_words:
+        text = '[{}] '.format(n_words) + text
+    elif compression:
+        text = '[{0:.1g}] '.format(compression) + text
+    x = tokenizer(text, return_tensors='pt', padding=True).to(summa_model.device)
+    with torch.inference_mode():
+        out = summa_model.generate(
+            **x, 
+            max_length=max_length, num_beams=num_beams, 
+            do_sample=do_sample, repetition_penalty=repetition_penalty, 
+            **kwargs
+        )
+    return tokenizer.decode(out[0], skip_special_tokens=True)
+
+# init asr model
 asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(
     model_name="stt_ru_conformer_ctc_large")
 
@@ -20,4 +54,8 @@ sf.write(file='/content/Audio1.wav', data = audio,
 audio_path = '/content/Audio1.wav'
 transcription = asr_model.transcribe([audio_path])
 
-print(transcription)
+# summarize
+summarization = summarize(transcription)
+
+# output
+print(summarization)
